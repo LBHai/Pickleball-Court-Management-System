0c43e82da85bab39ccf26a045ac6f629027d4443
HaLBH <halbhhe176636@fpt.edu.vn>
2025-02-13 01:07:59 +0700

    new

--- app/src/main/java/SEP490/G9/MainActivity.java
+++ app/src/main/java/SEP490/G9/MainActivity.java
@@ -3,12 +3,19 @@ package SEP490.G9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+        }
     }
-}
